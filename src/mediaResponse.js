2ddbcb9734be5abc55d643637dd687a94544109e
deb2610 <deb2610@rit.edu>
2020-01-30 18:09:15 -0500

    Clean up

--- src/mediaResponse.js
+++ src/mediaResponse.js
@@ -58,53 +58,6 @@ const getMedia = (request, response, contentType, mediaFile) => {
   // try it anyway
   loadVideo(request, response, contentType, mediaFile);
 };
-// START SAVE
-// const getParty = (request, response) => {
-//   const file = path.resolve(__dirname, '../client/party.mp4');
-
-//   fs.stat(file, (err, stats) => {
-//     if (err) {
-//       if (err.code === 'ENOENT') {
-//         response.writeHead(404);
-//       }
-//       return response.end(err);
-//     }
-//     let { range } = request.headers;
-//     if (!range) {
-//       range = 'bytes=0-';
-//     }
-//     const positions = range.replace(/bytes=/, '').split('-');
-
-//     let start = parseInt(positions[0], 10);
-//     const total = stats.size;
-//     const end = positions[1] ? parseInt(positions[1], 10) : total - 1;
-
-//     if (start > end) {
-//       start = end - 1;
-//     }
-//     const chunksize = (end - start) + 1;
-//     response.writeHead(206, {
-//       'Content-Range': `bytes ${start}-${end}/${total}`,
-//       'Accepted-Ranges': 'bytes',
-//       'Content-Length': chunksize,
-//       'Content-Type': 'video/mp4',
-//     });
-
-//     const stream = fs.createReadStream(file, { start, end });
-
-//     stream.on('open', () => {
-//       stream.pipe(response);
-//     });
-//     stream.on('error', (streamErr) => {
-//       stream.end(streamErr);
-//     });
-//     return stream;
-//   });
-//   // console.log(request.url);
-//   // response.write(sponge);
-//   // response.end();
-// };
-// END SAVE
 
 module.exports.getMedia = getMedia;
 module.exports.getParty = getParty;
