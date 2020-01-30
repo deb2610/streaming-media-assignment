227b9cffad051c1d62d13a18268bc71817564177
deb2610 <deb2610@rit.edu>
2020-01-30 18:05:28 -0500

    Asssignment Complete

--- src/htmlResponse.js
+++ src/htmlResponse.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 
 const index = fs.readFileSync(`${__dirname}/../client/client.html`);
+const page2 = fs.readFileSync(`${__dirname}/../client/client2.html`);
+const page3 = fs.readFileSync(`${__dirname}/../client/client3.html`);
 
 const getIndex = (request, response) => {
   console.log(request.url);
@@ -8,5 +10,19 @@ const getIndex = (request, response) => {
   response.write(index);
   response.end();
 };
+const getPage2 = (request, response) => {
+  console.log(request.url);
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.write(page2);
+  response.end();
+};
+const getPage3 = (request, response) => {
+  console.log(request.url);
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.write(page3);
+  response.end();
+};
 
 module.exports.getIndex = getIndex;
+module.exports.getPage2 = getPage2;
+module.exports.getPage3 = getPage3;
--- src/mediaResponse.js
+++ src/mediaResponse.js
@@ -1,10 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
-
-const getParty = (request, response) => {
-  const file = path.resolve(__dirname, '../client/party.mp4');
-
+const loadVideo = (request, response, contentType, mediaFile) => {
+  const file = path.resolve(__dirname, `../client/${mediaFile}`);
   fs.stat(file, (err, stats) => {
     if (err) {
       if (err.code === 'ENOENT') {
@@ -27,10 +25,10 @@ const getParty = (request, response) => {
     }
     const chunksize = (end - start) + 1;
     response.writeHead(206, {
-      'Content-Ranges': `bytes ${start}-${end}/${total}`,
+      'Content-Range': `bytes ${start}-${end}/${total}`,
       'Accepted-Ranges': 'bytes',
       'Content-Length': chunksize,
-      'Content-Type': 'video/mp4',
+      'Content-Type': contentType,
     });
 
     const stream = fs.createReadStream(file, { start, end });
@@ -43,9 +41,72 @@ const getParty = (request, response) => {
     });
     return stream;
   });
-  // console.log(request.url);
-  // response.write(sponge);
-  // response.end();
 };
+const getParty = (request, response) => {
+  // load party video
+  loadVideo(request, response, 'video/mp4', 'party.mp4');
+};
+const getBling = (request, response) => {
+  // load wii audio
+  loadVideo(request, response, 'audio/mpeg', 'bling.mp3');
+};
+const getBird = (request, response) => {
+  // load bird video
+  loadVideo(request, response, 'video/mp4', 'bird.mp4');
+};
+const getMedia = (request, response, contentType, mediaFile) => {
+  // try it anyway
+  loadVideo(request, response, contentType, mediaFile);
+};
+// START SAVE
+// const getParty = (request, response) => {
+//   const file = path.resolve(__dirname, '../client/party.mp4');
 
+//   fs.stat(file, (err, stats) => {
+//     if (err) {
+//       if (err.code === 'ENOENT') {
+//         response.writeHead(404);
+//       }
+//       return response.end(err);
+//     }
+//     let { range } = request.headers;
+//     if (!range) {
+//       range = 'bytes=0-';
+//     }
+//     const positions = range.replace(/bytes=/, '').split('-');
+
+//     let start = parseInt(positions[0], 10);
+//     const total = stats.size;
+//     const end = positions[1] ? parseInt(positions[1], 10) : total - 1;
+
+//     if (start > end) {
+//       start = end - 1;
+//     }
+//     const chunksize = (end - start) + 1;
+//     response.writeHead(206, {
+//       'Content-Range': `bytes ${start}-${end}/${total}`,
+//       'Accepted-Ranges': 'bytes',
+//       'Content-Length': chunksize,
+//       'Content-Type': 'video/mp4',
+//     });
+
+//     const stream = fs.createReadStream(file, { start, end });
+
+//     stream.on('open', () => {
+//       stream.pipe(response);
+//     });
+//     stream.on('error', (streamErr) => {
+//       stream.end(streamErr);
+//     });
+//     return stream;
+//   });
+//   // console.log(request.url);
+//   // response.write(sponge);
+//   // response.end();
+// };
+// END SAVE
+
+module.exports.getMedia = getMedia;
 module.exports.getParty = getParty;
+module.exports.getBling = getBling;
+module.exports.getBird = getBird;
